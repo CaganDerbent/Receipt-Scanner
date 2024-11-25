b4940fd65c63a37d2d978b6a3ad6f773c8ec315c
CaganDerbent <caganbey4545@gmail.com>
2024-11-25 17:58:26 +0300

    app.js updated

--- backend/api/app.js
+++ backend/api/app.js
@@ -13,23 +13,20 @@ const fontkit = require('@pdf-lib/fontkit');
 const { scanReceipt } = require('./Controllers/ScanController');
 const { createPdf } = require('./Controllers/FileController');
 
-
 const app = express();
 const port = 3001;
 
 app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "DELETE", "PUT","OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }));
-
+  origin: "*",
+  methods: ["GET", "POST", "DELETE", "PUT","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(bodyParser.json({ limit: '10mb' }));
 
-let filename = ``
-let id = ""
-let ext = ""
-
+let filename = ``;
+let id = "";
+let ext = "";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -40,10 +37,7 @@ const storage = multer.diskStorage({
     id = uuidv4();
     filename = `receipt_${id}${ext}`;
 
-    req.fileInfo = { filename,ext,id };
-
-
-
+    req.fileInfo = { filename, ext, id };
     cb(null, filename);
   }
 });
@@ -54,16 +48,12 @@ const upload = multer({
 });
 
 app.post('/process', upload.single('file'), scanReceipt);
-app.post('/pdf',upload.single('file'), createPdf);
+app.post('/pdf', upload.single('file'), createPdf);
 
-
-
-app.get("/",(req,res)=>{
-  res.send("hello world")
-})
-
-
-app.listen(port, () => {
-  console.log('Server is listening on port 3001');
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
+module.exports = (req, res) => {
+  app(req, res);
+};
